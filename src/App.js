f52297a1b4c4d91932496d0913ac5e90feb1fce8
sabachancoin <sabachancoin@gmail.com>
2019-01-09 20:31:12 +0900

    index and app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Ranking from './containers/Ranking';
+import Nav from './containers/Nav';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{paddingLeft: 240}}>
+        <Reboot/>
+        <AppBar style={{left: 240}}>
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Yahoo!　ショッピングランキング
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Nav/>
+        <div style={{marginTop: 64, padding: 32}}>
+          <Switch>
+            <Route path="/all" component={Ranking}/>
+            <Route path="/category/1" render={() => <Redirect to="/all"/>}/>
+            <Route path="/category/:id" render={({match}) => <Ranking categoryId={match.params.id}/>}/>
+          </Switch>
+        </div>
       </div>
     );
   }
