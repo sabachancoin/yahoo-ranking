1d5bb72c9e09256fbfbeb258cf86cd2e08c8f5a4
sabachancoin <sabachancoin@gmail.com>
2019-01-09 20:36:26 +0900

    Updates

---
+++ precache-manifest.61d2e2e93e18f8d67c14c6770fa021af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d1f246b4e561b8b7ec6",
+    "url": "/yahoo-ranking/static/js/runtime~main.2d1f246b.js"
+  },
+  {
+    "revision": "80bfcebbed4c34d9893f",
+    "url": "/yahoo-ranking/static/js/main.80bfcebb.chunk.js"
+  },
+  {
+    "revision": "4e1161b6cecf14f528e7",
+    "url": "/yahoo-ranking/static/js/1.4e1161b6.chunk.js"
+  },
+  {
+    "revision": "0d1a95bb94db3e3ed2d32140431cfc0d",
+    "url": "/yahoo-ranking/index.html"
+  }
+];
